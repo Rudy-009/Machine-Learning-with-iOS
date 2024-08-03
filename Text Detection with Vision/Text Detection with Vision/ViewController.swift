849afd1fa1f0d3c98ffa9c5d5a5584f850340ae7
Rudy-009 <rofxnaos@gmail.com>
2024-08-03 14:25:01 +0900

    Create: Text Detection with Vision Project

---
+++ Text Detection with Vision/Text Detection with Vision/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Text Detection with Vision
+//
+//  Created by 이승준 on 8/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
